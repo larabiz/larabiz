a077827b22f0963ca44441e2cdd235ea4db57fb5
Benjamin Crozat <hello@benjamincrozat.com>
2022-07-26 08:12:10 +0200

    Cleaned up status notification

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -15,22 +15,7 @@
         @endif
     </head>
     <body class="bg-indigo-50 text-gray-700">
-        @if (session('status'))
-            <div
-                class="bg-gradient-to-r from-gray-700 to-gray-800 fixed bottom-4 left-1/2 -translate-x-1/2 px-4 py-3 rounded-lg shadow-lg shadow-indigo-900/20 text-white"
-                x-data="{ open: true }"
-                x-show="open"
-            >
-                <div class="flex items-center gap-4">
-                    <div>{{ session('status') }}</div>
-
-                    <button class="text-indigo-400" @click="open = false">
-                        <span class="font-bold sm:sr-only">Fermer</span>
-                        <x-heroicon-o-x class="hidden sm:inline w-4 h-4" />
-                    </button>
-                </div>
-            </div>
-        @endif
+        <x-notification />
 
         <div class="flex flex-col min-h-screen">
             <x-nav />
