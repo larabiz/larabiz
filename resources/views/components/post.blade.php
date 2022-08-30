f61d158f32f3502aaf3cd5fa08485ae12a513515
Benjamin Crozat <hello@benjamincrozat.com>
2022-08-30 21:00:07 +0200

    Updated posts appearance

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -1,31 +1,35 @@
-<a href="{{ route('posts.show', [$post->random_id, $post->slug]) }}" class="border-t first:border-t-0 border-black/5 flex items-center justify-between gap-8 pt-8 first:pt-0">
-    <span>
-        <span class="font-bold leading-tight text-indigo-900 text-lg sm:text-xl">
+<a href="{{ route('posts.show', [$post->random_id, $post->slug]) }}" class="bg-white/50 flex items-center justify-between gap-6 p-6 rounded-lg shadow-lg shadow-indigo-100">
+    <div>
+        <div class="font-bold leading-tight text-indigo-900 text-lg sm:text-xl">
             {{ $post->title }}
-        </span>
+        </div>
 
-        <span class="flex flex-wrap gap-x-6 sm:gap-x-8 gap-y-2 mt-4 md:mt-2 text-indigo-900/50 text-sm sm:text-base">
-            <span class="flex items-center gap-2">
+        <div class="mt-2 text-indigo-700/50">
+            {{ $post->excerpt }}
+        </div>
+
+        <div class="flex flex-wrap gap-x-6 sm:gap-x-8 gap-y-2 mt-8 text-indigo-900/50 text-sm sm:text-base">
+            <div class="flex items-center gap-2">
                 <x-heroicon-o-calendar class="h-5 translate-y-[-1.5px]" />
                 <strong class="text-indigo-900">{{ $post->latestStatus()->created_at->isoFormat('ll') }}</strong>
-            </span>
+            </div>
 
-            <span class="flex items-center gap-2">
+            <div class="flex items-center gap-2">
                 <x-heroicon-o-user class="h-5 translate-y-[-1px]" />
                 <strong class="text-indigo-900">{{ $post->username }}</strong>
-            </span>
+            </div>
 
-            <span class="flex items-center gap-2">
-                <x-heroicon-o-clock class="h-5 translate-y-[-1.5px]" />
+            <div class="flex items-center gap-2">
+                <x-heroicon-o-clock class="h-5 translate-y-[-0.5px]" />
                 <strong class="text-indigo-900">{{ $post->read_time }} min</strong>
-            </span>
+            </div>
 
-            <span class="flex items-center gap-2">
+            <div class="flex items-center gap-2">
                 <x-heroicon-o-chat-bubble-left-right class="h-5 translate-y-[-1px]" />
                 @choice('<strong class="text-indigo-900">:count commentaire</strong>|<strong class="text-indigo-900">:count commentaires</strong>', $post->comments_count)
-            </span>
-        </span>
-    </span>
+            </div>
+        </div>
+    </div>
 
     <x-heroicon-o-chevron-right class="h-5 text-indigo-300/50" />
 </a>
