0114a01a6589d5e785760a5f4ff50e613c485d28
Benjamin Crozat <hello@benjamincrozat.com>
2022-08-31 17:47:07 +0200

    UI fix

--- resources/views/user/subscriptions.blade.php
+++ resources/views/user/subscriptions.blade.php
@@ -4,21 +4,23 @@
     </x-breadcrumb>
 
     <x-section>
-        @forelse ($user->subscriptions as $subscription)
-            <div class="bg-white/50 flex items-center justify-between gap-4 sm:gap-6 px-4 py-6 sm:p-6 rounded-lg shadow-lg shadow-indigo-200/50">
-                <p class="font-bold leading-tight text-indigo-900">
-                    {{ $subscription->subscribable->title }}
-                </p>
+        <div class="grid gap-4 sm:gap-8">
+            @forelse ($user->subscriptions as $subscription)
+                <div class="bg-white/50 flex items-center justify-between gap-4 sm:gap-6 px-4 py-6 sm:p-6 rounded-lg shadow-lg shadow-indigo-200/50">
+                    <p class="font-bold leading-tight text-indigo-900">
+                        {{ $subscription->subscribable->title }}
+                    </p>
 
-                <x-form method="POST" action="{{ route('unsubscribe-from-post', $subscription->subscribable) }}">
-                    <button type="submit" class="border border-red-100 hover:bg-red-100 flex items-center justify-center rounded-full transition-colors w-8 h-8">
-                        <x-heroicon-o-trash class="text-red-400 translate-y-[-1px] w-4 h-4" />
-                        <span class="sr-only">Supprimer</span>
-                    </button>
-                </x-form>
-            </div>
-        @empty
-            <p class="text-center text-indigo-300">Vous n'avez aucun abonnement en cours.</p>
-        @endforelse
+                    <x-form method="POST" action="{{ route('unsubscribe-from-post', $subscription->subscribable) }}">
+                        <button type="submit" class="border border-red-100 hover:bg-red-100 flex items-center justify-center rounded-full transition-colors w-8 h-8">
+                            <x-heroicon-o-trash class="text-red-400 translate-y-[-1px] w-4 h-4" />
+                            <span class="sr-only">Supprimer</span>
+                        </button>
+                    </x-form>
+                </div>
+            @empty
+                <p class="text-center text-indigo-300">Vous n'avez aucun abonnement en cours.</p>
+            @endforelse
+        </div>
     </x-section>
 </x-app>
