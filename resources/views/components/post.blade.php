bdc81b25a69995b7bbf83c10b6c8cf1074d7b35b
Benjamin Crozat <hello@benjamincrozat.com>
2022-08-31 09:59:41 +0200

    Fixed post component on smalls screens

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -1,4 +1,4 @@
-<a href="{{ route('posts.show', [$post->random_id, $post->slug]) }}" {{ $attributes->merge(['class' => 'bg-white/50 flex items-center justify-between gap-6 p-6 rounded-lg shadow-lg shadow-indigo-200/50']) }}>
+<a href="{{ route('posts.show', [$post->random_id, $post->slug]) }}" {{ $attributes->merge(['class' => 'bg-white/50 flex items-center justify-between gap-4 sm:gap-6 gap-6 px-4 py-6 sm:p-6 rounded-lg shadow-lg shadow-indigo-200/50']) }}>
     <div>
         <div class="font-bold leading-tight text-indigo-900 text-lg sm:text-xl">
             {{ $post->title }}
@@ -8,7 +8,7 @@
             {{ $post->excerpt }}
         </div>
 
-        <div class="flex flex-wrap gap-x-6 sm:gap-x-8 gap-y-2 mt-8 text-indigo-900/50 text-sm sm:text-base">
+        <div class="flex flex-wrap gap-x-6 sm:gap-x-8 gap-y-3 mt-8 text-indigo-900/50 text-sm sm:text-base">
             <div class="flex items-center gap-2">
                 <x-heroicon-o-calendar class="h-5 translate-y-[-1.5px]" />
                 <strong class="text-indigo-900">{{ $post->latestStatus()->created_at->isoFormat('ll') }}</strong>
