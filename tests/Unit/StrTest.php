a49373cc6fe485194a87c7c6c3caf8a3d145a3f8
Benjamin Crozat <hello@benjamincrozat.com>
2022-09-09 03:40:21 +0200

    Added more unit tests

--- tests/Unit/StrTest.php
+++ tests/Unit/StrTest.php
@@ -7,24 +7,40 @@ use Illuminate\Support\Str;
 
 class StrTest extends TestCase
 {
+    public function test_it_renders_headings_with_ids() : void
+    {
+        $this->assertStringContainsString(
+            'id="dabc-def"',
+            Str::marxdown("# D'abc def-._&")
+        );
+    }
+
+    public function test_it_renders_images_with_loading_attribute() : void
+    {
+        $this->assertStringContainsString(
+            'loading="lazy"',
+            Str::marxdown('![](https://example.com/some/image.jpg)')
+        );
+    }
+
     public function test_it_renders_external_links_with_appropriate_attributes() : void
     {
-        $this->assertEquals(
-            '<p><a rel="nofollow noopener noreferrer" target="_blank" href="https://www.apple.com">Apple</a></p>',
-            trim(Str::marxdown('[Apple](https://www.apple.com)'))
+        $this->assertStringContainsString(
+            'rel="nofollow noopener noreferrer" target="_blank"',
+            Str::marxdown('[Apple](https://www.apple.com)')
         );
     }
 
     public function test_it_renders_internal_links_without_target_and_rel_attributes() : void
     {
-        $this->assertEquals(
-            '<p><a href="https://larabiz.fr">Larabiz</a></p>',
-            trim(Str::marxdown('[Larabiz](https://larabiz.fr)'))
+        $this->assertStringContainsString(
+            '<a href="https://larabiz.fr">',
+            Str::marxdown('[Larabiz](https://larabiz.fr)')
         );
 
-        $this->assertEquals(
-            '<p><a href="#foo">Foo</a></p>',
-            trim(Str::marxdown('[Foo](#foo)'))
+        $this->assertStringContainsString(
+            '<a href="#foo">',
+            Str::marxdown('[Foo](#foo)')
         );
     }
 }
