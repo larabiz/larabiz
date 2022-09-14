2c6406e1eb84391856de9c7dc475e8c451bd7032
Benjamin Crozat <hello@benjamincrozat.com>
2022-09-14 11:11:44 +0200

    Fixed migration

--- database/migrations/2022_09_12_130010_add_views_column_to_posts_table.php
+++ database/migrations/2022_09_12_130010_add_views_column_to_posts_table.php
@@ -8,7 +8,7 @@ return new class extends Migration {
     public function up() : void
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->unsignedBigInteger('views')->default(0);
+            $table->unsignedBigInteger('views')->default(0)->after('seo_excerpt');
         });
     }
 
